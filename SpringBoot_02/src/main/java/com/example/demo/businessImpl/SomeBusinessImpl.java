717aaa8eb65d69afad3d1ae59186d71ea41eb63a
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-19 12:15:30 +0530

    1st basic example of junit

---
+++ SpringBoot_02/src/main/java/com/example/demo/businessImpl/SomeBusinessImpl.java
@@ -0,0 +1,14 @@
+package com.example.demo.businessImpl;
+
+public class SomeBusinessImpl {
+	public int calculateSum(int[] data) {
+		int sum = 0;
+		for(int value : data) {
+			if(value < 0) {
+				sum += value;
+			}
+			return sum;
+		}
+		return sum;
+	}
+}
---
+++ SpringBoot_02/src/test/java/com/example/demo/SomeBusinessTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.businessImpl.SomeBusinessImpl;
+
+class SomeBusinessTest {
+
+	@Test
+	public void testCalculateSum() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] {1, 2, 3});
+		int expectedResult = 6;
+		assertEquals(expectedResult, actualResult);
+	}
+
+}
