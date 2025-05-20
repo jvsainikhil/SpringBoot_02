f380fab4e7ceecf92992ae7dad1841daea6e0cff
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-20 22:21:50 +0530

    Test After Stabbing

--- SpringBoot_02/src/test/java/com/example/demo/SomeBusinessTest.java
+++ SpringBoot_02/src/test/java/com/example/demo/SomeBusinessTest.java
@@ -55,10 +55,11 @@ class SomeBusinessTest {
 		assertEquals(10, actualResult);
 	}
 	@Test
-	public void testCalculateSum() { // test with out stabbing 
+	public void testCalculateSum() { // test with after stabbing 
 		SomeBusinessImpl business = new SomeBusinessImpl();
-		int actualResult = business.calculateSum(new int[] {1, 2, 3});
-		int expectedResult = 6;
+		business.setSomeDataService(new SomeDataServiceBigStub());
+		int actualResult = business.calculateSumUsingDataService();
+		int expectedResult = 10;
 		assertEquals(expectedResult, actualResult);
 	}
 	
