bb16c9d84da4ef4d9fcdec5636ac142912d6fcb1
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-24 23:59:25 +0530

    step1_00

--- SpringBoot_02/src/test/java/com/example/demo/SomeBusinessMockTest.java
+++ SpringBoot_02/src/test/java/com/example/demo/SomeBusinessMockTest.java
@@ -10,11 +10,11 @@ import com.example.demo.unittesting.business.SomeBusinessImpl;
 import com.example.demo.unittesting.data.SomeDataService;
 
 class SomeBusinessMockTest {	
+	SomeBusinessImpl business = new SomeBusinessImpl();//This line in common in all the test cases
+	SomeDataService dataServiceMock= mock(SomeDataService.class);//This line in common in all the test cases
 	
 	@Test
 	public void calculateSum_empty()  {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		SomeDataService dataServiceMock= mock(SomeDataService.class);
 		//we want return this --> dataServiceMock retriveAllData new int[] {1,2,3} --> need to define it 
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {1,2,3});
 		business.setSomeDataService(dataServiceMock);
@@ -23,8 +23,6 @@ class SomeBusinessMockTest {
 	}
 	@Test
 	public void calculateSumUsingDataService_basic() {
-		SomeBusinessImpl business = new SomeBusinessImpl();	
-		SomeDataService dataServiceMock= mock(SomeDataService.class);
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] { });//we want return
 		business.setSomeDataService(dataServiceMock);
 		int actualResult = business.calculateSumUsingDataService();
@@ -32,8 +30,6 @@ class SomeBusinessMockTest {
 	}
 	@Test
 	public void calculateSum_empty01()  {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		SomeDataService dataServiceMock= mock(SomeDataService.class);
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {4});
 		business.setSomeDataService(dataServiceMock);
 		int actualResult = business.calculateSumUsingDataService();
@@ -41,8 +37,6 @@ class SomeBusinessMockTest {
 	}
 	@Test
 	public void testCalculateSum() { // test with after stabbing 
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		SomeDataService dataServiceMock= mock(SomeDataService.class);
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {1,2,3,4,1});
 		business.setSomeDataService(dataServiceMock);
 		int actualResult = business.calculateSumUsingDataService();
