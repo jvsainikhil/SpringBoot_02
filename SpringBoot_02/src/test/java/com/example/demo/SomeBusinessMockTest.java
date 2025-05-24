a3d97da397f8a656945d3f20ba68d54f28370df0
jvsnikhil <114561659+jvsainikhil@users.noreply.github.com>
2025-05-25 00:11:03 +0530

    Step1_02

--- SpringBoot_02/src/test/java/com/example/demo/SomeBusinessMockTest.java
+++ SpringBoot_02/src/test/java/com/example/demo/SomeBusinessMockTest.java
@@ -25,28 +25,28 @@ class SomeBusinessMockTest {
 	public void calculateSum_empty()  {
 		//we want return this --> dataServiceMock retriveAllData new int[] {1,2,3} --> need to define it 
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {1,2,3});
-		business.setSomeDataService(dataServiceMock);//to add this for all is @BeforeEach 
-		int actualResult = business.calculateSumUsingDataService();
-		assertEquals(6, actualResult);
+		//business.setSomeDataService(dataServiceMock);//to add this for all is @BeforeEach 
+		assertEquals(6, business.calculateSumUsingDataService());
 	}
 	@Test
 	public void calculateSumUsingDataService_basic() {
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] { });//we want return
-		business.setSomeDataService(dataServiceMock);
-		int actualResult = business.calculateSumUsingDataService();
-		assertEquals(0, actualResult);
+		//business.setSomeDataService(dataServiceMock);
+		assertEquals(0, business.calculateSumUsingDataService());
 	}
 	@Test
 	public void calculateSum_empty01()  {
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {4});
-		business.setSomeDataService(dataServiceMock);
-		int actualResult = business.calculateSumUsingDataService();
-		assertEquals(4, actualResult);
+		//business.setSomeDataService(dataServiceMock);
+		assertEquals(4, business.calculateSumUsingDataService());
 	}
+	
+	// have used
+	// --> Hover or click on object and then --> Refactor --> inline --> (Alt+Shift+I).
 	@Test
 	public void testCalculateSum() { // test with after stabbing 
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {1,2,3,4,1});
-		business.setSomeDataService(dataServiceMock);
+		//business.setSomeDataService(dataServiceMock);
 		int actualResult = business.calculateSumUsingDataService();
 		int expectedResult = 11;
 		assertEquals(expectedResult, actualResult);
